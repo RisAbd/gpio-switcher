c87147f8bda7a8020442bf3d0447bf7a3e755a4d
Abdullah Ibragimov <irsalabd@gmail.com>
2019-12-08 16:58:20 +0000

    refactor /toggle/ handling, prettify html

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os, sys
 import socketserver
 from http.server import BaseHTTPRequestHandler
 import shutil
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs
 import pathlib
 import jinja2
 from collections import namedtuple
@@ -15,18 +15,7 @@ TEMPLATES_DIRS = [pathlib.Path('templates/'), ]
 jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=TEMPLATES_DIRS))
 
 
-def switch(handler):
-    p = pathlib.Path('/sys/class/gpio/gpio1/value') 
-    if p.read_text().strip() == '1':
-        p.write_text('0')
-    else:
-        p.write_text('1')
-    handler.send_response(301)
-    handler.send_header('Location', '/')
-    handler.end_headers()
-
-
-class Switch(namedtuple('Switch', 'id name gpio_pin')):
+class Switch(namedtuple('Switch', 'id name gpio_pin reversed')):
     @property
     def value_file(self):
         return pathlib.Path('/sys/class/gpio/gpio{}/value'.format(self.gpio_pin))
@@ -35,28 +24,54 @@ class Switch(namedtuple('Switch', 'id name gpio_pin')):
     def value(self):
         return int(self.value_file.read_text())
 
+    @value.setter
+    def value(self, v):
+        return self.value_file.write_text(str(v))
+
+    def toggle_value(self):
+        self.value = abs(self.value - 1)
+
     @property
     def state(self):
         try:
             v = self.value
         except OSError:
-            return 'unconfigured'
+            return 'misconfigured'
         except ValueError:
             return 'unknown'
 
-        if v == 0:
-            return 'on'
-        elif v == 1:
-            return 'off'
+        combs_on = ((1, False), (0, True))
+        val_rev = (v, self.reversed)
+
+        if v in (1, 0):
+            return 'on' if val_rev in combs_on else 'off'
         else: 
             return 'unknown'
 
 
 def get_switches():
     return [
-            Switch(1, 'Lamp #1', 1),
-            Switch(2, 'Lamp #2', 2),
+            Switch(1, 'Lamp', 1, False),
+            Switch(2, 'LED', 10, False),
+            Switch(-1, 'Kek', -1, False),
             ]
+
+
+def toggle_switch(handler):
+    clen = int(handler.headers.get('Content-Length'))
+    req_body = handler.rfile.read(clen)
+    p = parse_qs(req_body)
+    
+    sw_id = int(p.get(b'switch_id', [-1, ])[0])
+    switch = next(filter(lambda s: s.id == sw_id, get_switches()), None)
+    if not switch:
+        return handle_404(handler)
+
+    switch.toggle_value()
+
+    handler.send_response(301)
+    handler.send_header('Location', '/')
+    handler.end_headers()
 
 
 def index(handler):
@@ -75,14 +90,18 @@ def index(handler):
 
 
 def handle_404(handler):
+    msg = b'Not Found\n'
     handler.send_response(404)
+    handler.send_header('Content-Type', 'text/html')
+    handler.send_header('Content-Length', len(msg))
     handler.end_headers()
+    handler.wfile.write(msg)
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        if self.path.startswith('/switch'):
-            switch(self)
+        if self.path.startswith('/toggle/'):
+            toggle_switch(self)
         else:
             handle_404(self)
 	
